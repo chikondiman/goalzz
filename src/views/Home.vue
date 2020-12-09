ca23598ce4fbb115006c63987a109a239286707f
chikondi <chikondiman@gmail.com>
2020-12-09 13:15:11 -0600

    init commit

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: "Goalz: the secret weapon to ruling the world.",
+    };
+  },
+  created: function() {},
+  methods: {},
+};
+</script>
