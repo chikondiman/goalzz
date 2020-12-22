3c129a6218c6f4217c5fae020ebc14d75ce3b464
chikondi <chikondiman@gmail.com>
2020-12-22 12:53:02 -0500

    goal counter adjustment

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,10 +10,24 @@
 export default {
   data: function() {
     return {
-      message: "Goalz: the secret weapon to ruling the world.",
+      message: "Goalz: the secret weapon to ruling the world.", 
+message: "Goal Count = 1",
+
+
+count: 0
+
+
+
+
+      
     };
   },
   created: function() {},
   methods: {},
 };
 </script>
+
+
+
+
+// Define a new component called button-counter
